5e0c2e47a9adcdb7f8537aa2a9fe7e0dc39c9d2e
brodie02 <marshallbrodie0@gmail.com>
2022-10-13 20:44:37 +1030

    Add amount parameter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Exchange Rate Api
-var exchangeUrl = "https://v6.exchangerate-api.com/v6/9ac30c9d3885914f5e6affff/pair/AUD/USD"
+var exchangeUrl = "https://v6.exchangerate-api.com/v6/9ac30c9d3885914f5e6affff/pair/AUD/USD/5"
 
 fetch(exchangeUrl)
     .then(function(response) {
