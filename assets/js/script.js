689e4af25944cf7c7d65548699a972cfdd322ef0
brodie02 <marshallbrodie0@gmail.com>
2022-10-17 19:33:40 +1030

    Add modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+  });
+
 // Exchange Rate Api
 var exchangeUrl = "https://v6.exchangerate-api.com/v6/9ac30c9d3885914f5e6affff/pair/AUD/USD/5"
 
-fetch(exchangeUrl)
+fetch(exchangeUrl) 
     .then(function(response) {
         return response.json()
     })
