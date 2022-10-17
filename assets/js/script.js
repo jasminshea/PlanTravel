bff9a6b882b8272e4345c7811e7cc186ea3a96b7
brodie02 <marshallbrodie0@gmail.com>
2022-10-17 21:17:17 +1030

    Add dropdown tabs

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Modal.init(elems);
   });
 
+
 // Exchange Rate API
 var exchangeUrl = "https://v6.exchangerate-api.com/v6/9ac30c9d3885914f5e6affff/pair/AUD/USD/5"
 
