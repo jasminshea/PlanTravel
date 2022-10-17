c456c9488f08aba55f32650785786a32cf4a8f25
brodie02 <marshallbrodie0@gmail.com>
2022-10-17 20:15:12 +1030

    Add working datepicker

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
+// Datepicker Function
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems);
+  });
+
+// Modal Function
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems);
   });
 
-// Exchange Rate Api
+// Exchange Rate API
 var exchangeUrl = "https://v6.exchangerate-api.com/v6/9ac30c9d3885914f5e6affff/pair/AUD/USD/5"
 
 fetch(exchangeUrl) 
