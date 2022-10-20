6a73d434d2059d4b7fd6b803881f86b772364b30
brodie02 <marshallbrodie0@gmail.com>
2022-10-20 19:20:09 +1030

    Declare modal inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,13 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Modal.init(elems);
   });
 
+// Linking both pages 
+document.getElementById('continue').addEventListener('click', function() {
+  var start = document.getElementById('startloc').value
+  var dest = document.getElementById('dest').value
+  var city = document.getElementById('city').value
+  var date = document.getElementById('date').value
+})
 
 // Exchange Rate API
 //var exchangeUrl = "https://v6.exchangerate-api.com/v6/9ac30c9d3885914f5e6affff/pair/AUD/USD/5"
